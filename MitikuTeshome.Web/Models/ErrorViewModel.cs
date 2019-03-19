603e8737edeca612ca9d0d53404945cf45ed9b6e
Mitiku Geleta <mitikutesh@gmail.com>
2019-03-19 19:57:30 +0200

    Add project files.

---
+++ MitikuTeshome.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MitikuTeshome.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
